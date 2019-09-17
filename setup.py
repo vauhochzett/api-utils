fd1e70217a091b5a5b740cc379edd7d7c19d68e6
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-17 17:14:12 +0200

    Adds setup.py with all basic information

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="api-utils",
+    version="2019.09",
+    description="API utils simplify life when creating or consuming APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
