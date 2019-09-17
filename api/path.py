30892f769db37f37c020b1191f85501c033b9480
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-17 17:29:13 +0200

    Formats code with black

--- api/path.py
+++ api/path.py
@@ -3,8 +3,9 @@
 
 from typing import List, Tuple
 
-def join(first:str, *others:str) -> str:
-	"""
+
+def join(first: str, *others: str) -> str:
+    """
 	Join one or more path components intelligently.
 	Contrary to `os.path`, there are no "absolute paths".
 	Rules:
@@ -15,31 +16,26 @@ def join(first:str, *others:str) -> str:
 	- Caution: "http://" will be stripped of its slashes! Instead pass "http://www.de" or similar.
 	"""
 
-	s = "/"
-	strip = lambda p: p.strip(s)
+    s = "/"
+    strip = lambda p: p.strip(s)
 
-	all_components:List[str] = [
-		p for p in [first, *others]
-		if p != ""
-	]
+    all_components: List[str] = [p for p in [first, *others] if p != ""]
 
-	if not all_components:
-		return ""
+    if not all_components:
+        return ""
 
-	start_s:bool = not all_components[0].startswith("http")
-	end_s:bool = all_components[-1] == s
+    start_s: bool = not all_components[0].startswith("http")
+    end_s: bool = all_components[-1] == s
 
-	# Filter empty strings again; this ensures that "/" and "//" do not result in double slashes
-	stripped_components:List[str] = [
-		p for p in [
-			strip(p) for p in all_components
-		] if p != ""
-	]
-	result = s.join(stripped_components)
+    # Filter empty strings again; this ensures that "/" and "//" do not result in double slashes
+    stripped_components: List[str] = [
+        p for p in [strip(p) for p in all_components] if p != ""
+    ]
+    result = s.join(stripped_components)
 
-	if start_s:
-		result = s + result
-	if end_s:
-		result = result + s
+    if start_s:
+        result = s + result
+    if end_s:
+        result = result + s
 
-	return result
+    return result
